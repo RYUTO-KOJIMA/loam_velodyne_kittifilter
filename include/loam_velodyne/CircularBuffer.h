4eb436a7d224bfb6428b0a058fe9b01de91360bf
Stern Gerlach <std.experimental.optional@gmail.com>
2021-09-01 21:18:54 +0900

    Only formatting fixes for CircularBuffer

--- include/loam_velodyne/CircularBuffer.h
+++ include/loam_velodyne/CircularBuffer.h
@@ -1,130 +1,131 @@
-#ifndef LOAM_CIRCULARBUFFER_H
-#define LOAM_CIRCULARBUFFER_H
+
+// CircularBuffer.h
+
+#ifndef LOAM_CIRCULAR_BUFFER_H
+#define LOAM_CIRCULAR_BUFFER_H
 
 #include <cstdlib>
 #include <vector>
 
-
 namespace loam {
-
-
 
 /** \brief Simple circular buffer implementation for storing data history.
  *
  * @tparam T The buffer element type.
  */
 template <class T>
-class CircularBuffer {
+class CircularBuffer
+{
 public:
-  CircularBuffer(const size_t& capacity = 200)
-        : _capacity(capacity),
-          _size(0),
-          _startIdx(0)
-  {
-    _buffer = new T[capacity];
-  };
+    CircularBuffer(const std::size_t capacity = 200) :
+        _capacity(capacity),
+        _size(0),
+        _startIdx(0),
+        _buffer(new T[capacity]) { }
 
-  ~CircularBuffer()
-  {
-    delete[] _buffer;
-    _buffer = NULL;
-  }
-
-  /** \brief Retrieve the buffer size.
-   *
-   * @return the buffer size
-   */
-  const size_t& size() {
-    return _size;
-  }
-
-  /** \brief Retrieve the buffer capacity.
-   *
-   * @return the buffer capacity
-   */
-  const size_t& capacity() {
-    return _capacity;
-  }
-
-  /** \brief Ensure that this buffer has at least the required capacity.
-   *
-   * @param reqCapacity the minimum required capacity
-   */
-  void ensureCapacity(const int& reqCapacity) {
-    if (reqCapacity > 0 && _capacity < reqCapacity) {
-      // create new buffer and copy (valid) entries
-      T* newBuffer = new T[reqCapacity];
-      for (size_t i = 0; i < _size; i++) {
-        newBuffer[i] = (*this)[i];
-      }
-
-      // switch buffer pointers and delete old buffer
-      T* oldBuffer = _buffer;
-      _buffer = newBuffer;
-      _startIdx = 0;
-
-      delete[] oldBuffer;
+    ~CircularBuffer()
+    {
+        delete[] this->_buffer;
+        this->_buffer = nullptr;
     }
-  }
 
-  /** \brief Check if the buffer is empty.
-   *
-   * @return true if the buffer is empty, false otherwise
-   */
-  bool empty() {
-    return _size == 0;
-  }
+    /** \brief Retrieve the buffer size.
+     *
+     * @return The buffer size
+     */
+    inline std::size_t size() const { return this->_size; }
 
-  /** \brief Retrieve the i-th element of the buffer.
-   *
-   *
-   * @param i the buffer index
-   * @return the element at the i-th position
-   */
-  const T& operator[](const size_t& i) {
-    return _buffer[(_startIdx + i) % _capacity];
-  }
+    /** \brief Retrieve the buffer capacity.
+     *
+     * @return The buffer capacity
+     */
+    inline std::size_t capacity() const { return this->_capacity; }
 
-  /** \brief Retrieve the first (oldest) element of the buffer.
-   *
-   * @return the first element
-   */
-  const T& first() {
-    return _buffer[_startIdx];
-  }
+    /** \brief Ensure that this buffer has at least the required capacity.
+     *
+     * @param reqCapacity The minimum required capacity
+     */
+    void ensureCapacity(const int reqCapacity)
+    {
+        if (reqCapacity <= 0 || this->_capacity >= reqCapacity)
+            return;
 
-  /** \brief Retrieve the last (latest) element of the buffer.
-   *
-   * @return the last element
-   */
-  const T& last() {
-    size_t idx = _size == 0 ? 0 : (_startIdx + _size - 1) % _capacity;
-    return _buffer[idx];
-  }
+        // Create new buffer and copy (valid) entries
+        T* newBuffer = new T[reqCapacity];
+        for (std::size_t i = 0; i < this->_size; ++i)
+            newBuffer[i] = this->at(i);
 
-  /** \brief Push a new element to the buffer.
-   *
-   * If the buffer reached its capacity, the oldest element is overwritten.
-   *
-   * @param element the element to push
-   */
-  void push(const T& element) {
-    if (_size < _capacity) {
-      _buffer[_size] = element;
-      _size++;
-    } else {
-      _buffer[_startIdx] = element;
-      _startIdx = (_startIdx + 1) % _capacity;
+        // Switch buffer pointers and delete old buffer
+        T* oldBuffer = this->_buffer;
+        this->_buffer = newBuffer;
+        this->_startIdx = 0;
+
+        delete[] oldBuffer;
     }
-  }
+
+    /** \brief Check if the buffer is empty.
+     *
+     * @return true if the buffer is empty, false otherwise
+     */
+    inline bool empty() const { return this->_size == 0; }
+
+    /** \brief Retrieve the i-th element of the buffer.
+     *
+     * @param i The buffer index
+     * @return The element at the i-th position
+     */
+    inline const T& operator[](const std::size_t i) const
+    { return this->_buffer[this->toBufferIndex(i)]; }
+
+    inline const T& at(const std::size_t i) const
+    { return this->_buffer[this->toBufferIndex(i)]; }
+
+    /** \brief Retrieve the first (oldest) element of the buffer.
+     *
+     * @return The first element
+     */
+    inline const T& first() const
+    { return this->_buffer[this->toBufferIndex(0)]; }
+
+    /** \brief Retrieve the last (latest) element of the buffer.
+     *
+     * @return the last element
+     */
+    inline const T& last() const
+    { return this->_buffer[this->toBufferIndex(this->_size - 1)]; }
+
+    /** \brief Push a new element to the buffer.
+     *
+     * If the buffer reached its capacity, the oldest element is overwritten.
+     *
+     * @param element The element to push
+     */
+    void push(const T& element)
+    {
+        if (this->_size < this->_capacity) {
+            this->_buffer[this->_size] = element;
+            this->_size++;
+        } else {
+            this->_buffer[this->_startIdx] = element;
+            this->_startIdx = (this->_startIdx + 1) % this->_capacity;
+        }
+    }
 
 private:
-  size_t _capacity;   ///< buffer capacity
-  size_t _size;       ///< current buffer size
-  size_t _startIdx;   ///< current start index
-  T* _buffer;         ///< internal element buffer
+    inline std::size_t toBufferIndex(const std::size_t idx) const
+    { return (this->_startIdx + idx) % this->_capacity; }
+
+private:
+    // Buffer capacity
+    std::size_t _capacity;
+    // Current buffer size
+    std::size_t _size;
+    // Current start index
+    std::size_t _startIdx;
+    // Internal element buffer
+    T*          _buffer;
 };
 
-} // end namespace loam
+} // namespace loam
 
-#endif //LOAM_CIRCULARBUFFER_H
+#endif //LOAM_CIRCULAR_BUFFER_H
