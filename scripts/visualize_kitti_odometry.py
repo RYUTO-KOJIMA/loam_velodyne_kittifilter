d11ce9f454a69eb88d14845eab4c6d94e3c0f579
Stern Gerlach <std.experimental.optional@gmail.com>
2021-09-11 18:03:07 +0900

    Add a script to plot the Kitti odometry results

---
+++ scripts/visualize_kitti_odometry.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# visualize_kitti_odometry.py
+
+# Command-line tool to visualize the Kitti odometry results obtained from the
+# loam_velodyne package
+
+import argparse
+import json
+import sys
+
+import pykitti
+import pykitti.utils
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_odometry(file_name: str, topic: str):
+    # Open the odometry file
+    f = open(file_name, "r", encoding="utf-8")
+    odometry_json = json.load(f)
+    # Close the odometry file
+    f.close()
+
+    poses_json = odometry_json[topic]
+    poses = []
+
+    for pose_json in poses_json["Results"]:
+        values = list(map(float, pose_json.split()))
+        _, trans_x, trans_y, trans_z, rot_x, rot_y, rot_z = values
+
+        # Create a 3x1 translation vector
+        t = np.array([trans_z, trans_x, trans_y])
+        # Create a 3x3 rotation matrix
+        Rx = pykitti.utils.rotx(rot_x)
+        Ry = pykitti.utils.roty(rot_y)
+        Rz = pykitti.utils.rotz(rot_z)
+        R = Ry.dot(Rx.dot(Rz))
+        # Create a 4x4 transformation matrix
+        trans = pykitti.utils.transform_from_rot_trans(R, t)
+
+        # Append the transformation
+        poses.append(trans)
+
+    return poses
+
+def main():
+    # Setup command-line options
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_dir", required=True, type=str)
+    parser.add_argument("--sequence", required=True, type=str)
+    parser.add_argument("--odometry", required=True, type=str)
+    parser.add_argument("--topic", required=True, type=str)
+    parser.add_argument("--xmin", default=None, type=float)
+    parser.add_argument("--ymin", default=None, type=float)
+    parser.add_argument("--xmax", default=None, type=float)
+    parser.add_argument("--ymax", default=None, type=float)
+    parser.add_argument("--legend_ncol", default=1, type=int)
+    parser.add_argument("--legend_bbox", default="1.05,1.0", type=str)
+    parser.add_argument("--legend_loc", default="upper left", type=str)
+
+    # Parse command-line arguments
+    args = parser.parse_args()
+    args.legend_bbox = tuple(map(float, args.legend_bbox.split(",")))
+
+    if args.topic not in ("Odometry", "Mapping", "Integrated"):
+        print("Topic name should be `Odometry`, `Mapping`, or `Integrated`")
+        sys.exit(1)
+
+    # Load the odometry data
+    dataset = pykitti.odometry(args.dataset_dir, args.sequence)
+
+    # Load the odometry results from the loam_velodyne package
+    odometry = load_odometry(args.odometry, args.topic)
+    # Transform the odometry poses to the rectified camera coordinate
+    odometry = [dataset.calib.T_cam0_velo.dot(x) for x in odometry]
+
+    # Setup figure
+    fig = plt.figure()
+    ax = fig.add_subplot()
+
+    # Ground-truth poses are available for sequences 0 to 10
+    if int(args.sequence) < 11:
+        # Get the ground-truth poses
+        ground_truth = np.array([(x[0,3], x[2,3]) for x in dataset.poses])
+        # Plot the ground-truth poses
+        ax.plot(ground_truth[:,0], ground_truth[:,1], label="Ground-truth")
+    else:
+        print("Ground-truth poses are not available for sequence {}"
+              .format(args.sequence))
+
+    # Plot the odometry results
+    poses = np.array([(x[0,3], x[2,3]) for x in odometry])
+    ax.plot(poses[:,0], poses[:,1], label="LOAM ({})".format(args.topic))
+
+    ax.set_xlabel("X (m)")
+    ax.set_ylabel("Y (m)")
+    ax.set_aspect("equal")
+
+    ax_x_min, ax_x_max = ax.get_xlim()
+    ax_y_min, ax_y_max = ax.get_ylim()
+    x_min = args.xmin if args.xmin is not None else ax_x_min
+    x_max = args.xmax if args.xmax is not None else ax_x_max
+    y_min = args.ymin if args.ymin is not None else ax_y_min
+    y_max = args.ymax if args.ymax is not None else ax_y_max
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+
+    ax.legend(ncol=args.legend_ncol, bbox_to_anchor=args.legend_bbox,
+              loc=args.legend_loc, borderaxespad=0.0, edgecolor="black",
+              fancybox=False, framealpha=1.0)
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
